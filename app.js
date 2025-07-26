c9b662dfdfc5e229305c06235c266162646beffa
raurus <190369371+fufu-fafa@users.noreply.github.com>
2025-07-26 13:17:29 +0700

    change port

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.get('/destinations/:id/metrics', async (req, res) => {
 });
 
 // Boot the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Read‑only API listening on http://localhost:${PORT}`);
 });
