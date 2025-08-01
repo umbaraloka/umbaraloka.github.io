abfff316ee188ead34d883135d9bfd3cae3f6eca
raurus <190369371+fufu-fafa@users.noreply.github.com>
2025-08-01 21:59:17 +0700

    final, hopefully

--- app.js
+++ app.js
@@ -17,13 +17,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
 app.get('/destination', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'destination.html'));
 });
-
 app.get('/about', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+app.get('/sumatra', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'sumatra.html'));
+});
+app.get('/kalimantan', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'kalimantan.html'));
+});
+app.get('/sulawesi', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'sulawesi.html'));
+});
+app.get('/papua', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'papua.html'));
 });
 
 // Serve pricing.json at /pricing
