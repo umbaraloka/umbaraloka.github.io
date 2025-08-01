c8498bd827611b0cc8a0b604444beba843fece45
fufu-fafa <190369371+fufu-fafa@users.noreply.github.com>
2025-08-01 08:29:26 +0700

    fix

--- app.js
+++ app.js
@@ -13,9 +13,17 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/:page?', (req, res, next) => {
-  let page = req.params.page || 'index';
-  res.sendFile(path.join(__dirname, 'public', `${page}.html`));
+// Serve index.html for "/" explicitly
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/destination', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'destination.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
 // Serve pricing.json at /pricing
